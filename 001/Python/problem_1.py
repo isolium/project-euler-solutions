752957bddd5afd7ff964a3646381b08bfeed7c22
isolium <mr.tester.2004@gmail.com>
2023-02-02 18:17:02 +0330

    Fix time calculating in Python solution for problem 001

--- 001/Python/problem_1.py
+++ 001/Python/problem_1.py
@@ -8,7 +8,7 @@ for i in range(1, 1000):
         summation += i
 print("- Solution 1 -")
 print("RESULT: {}".format(summation))
-exec_time = round((time.perf_counter()-start_time) * (10**6), 1)
+exec_time = round((time.perf_counter()-start_time) * (10**3), 1)
 print("TIME: {}ms".format(exec_time))
 
 print()
@@ -19,5 +19,5 @@ def sum_divisible_by(n, p):
     return n*(p//n)*((p//n)+1)//2
 print("- Solution 2 -")
 print("RESULT: {}".format(sum_divisible_by(3, 999) + sum_divisible_by(5, 999) - sum_divisible_by(15, 999)))
-exec_time = round((time.perf_counter()-start_time) * (10**6), 1)
+exec_time = round((time.perf_counter()-start_time) * (10**3), 1)
 print("TIME: {}ms".format(exec_time))
